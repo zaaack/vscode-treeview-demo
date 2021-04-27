152b83e846a378742403830fc9f9f5b7e25e2061
zaaack <zaaack@qq.com>
2021-04-27 20:13:54 +0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,42 @@
+'use strict'
+
+import * as vscode from 'vscode'
+
+import { DepNodeProvider, Dependency } from './nodeDependencies'
+
+export function activate(context: vscode.ExtensionContext) {
+  // Samples of `window.registerTreeDataProvider`
+  const nodeDependenciesProvider = new DepNodeProvider(
+    vscode.workspace.rootPath
+  )
+  vscode.window.registerTreeDataProvider(
+    'nodeDependencies',
+    nodeDependenciesProvider
+  )
+  vscode.commands.registerCommand('nodeDependencies.refreshEntry', () =>
+    nodeDependenciesProvider.refresh()
+  )
+  vscode.commands.registerCommand('extension.openPackageOnNpm', (moduleName) =>
+    vscode.commands.executeCommand(
+      'vscode.open',
+      vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)
+    )
+  )
+  vscode.commands.registerCommand('nodeDependencies.addEntry', () =>
+    vscode.window.showInformationMessage(`Successfully called add entry.`)
+  )
+  vscode.commands.registerCommand(
+    'nodeDependencies.editEntry',
+    (node: Dependency) =>
+      vscode.window.showInformationMessage(
+        `Successfully called edit entry on ${node.label}.`
+      )
+  )
+  vscode.commands.registerCommand(
+    'nodeDependencies.deleteEntry',
+    (node: Dependency) =>
+      vscode.window.showInformationMessage(
+        `Successfully called delete entry on ${node.label}.`
+      )
+  )
+}
